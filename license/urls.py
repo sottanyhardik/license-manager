1f6d01306896209acbf4b9908bb29dbc0fbc779e
Hardik Sottany <hardiksottany@Hardiks-MacBook-Pro.local>
2020-01-16 12:25:58 +0530

    Hardik: Version 5.02

--- license/urls.py
+++ license/urls.py
@@ -29,9 +29,8 @@ urlpatterns = [
          name='report_new_confectionery_other'),
     path('report/all/old', login_required(views.PDFOldAllReportView.as_view()), name='report_all_old'),
     path('biscuits/amend', login_required(views.BiscuitsAmmendmentView.as_view()), name='biscuit_amend'),
-    path('biscuits/new/expiried', login_required(views.PDFBiscuitsNewExpiryReportView.as_view()), name='biscuits_expiried'),
-    path('confectinery/new/expiried', login_required(views.PDFConfectioneryNewExpiredReportView.as_view()), name='confectinery_expiried'),
-    path('biscuits/old/expiried', login_required(views.PDFBiscuitsOldExpiryReportView.as_view()), name='biscuits_expiried'),
-    path('confectinery/old/expiried', login_required(views.PDFConfectioneryOldExpiredReportView.as_view()), name='confectinery_expiried'),
-
+    path('biscuits/new/expiried', login_required(views.PDFBiscuitsNewExpiryReportView.as_view()), name='biscuits_expiried_new'),
+    path('confectinery/new/expiried', login_required(views.PDFConfectioneryNewExpiredReportView.as_view()), name='confectinery_expiried_new'),
+    path('biscuits/old/expiried', login_required(views.PDFBiscuitsOldExpiryReportView.as_view()), name='biscuits_expiried_old'),
+    path('confectinery/old/expiried', login_required(views.PDFConfectioneryOldExpiredReportView.as_view()), name='confectinery_expiried_old'),
 ]

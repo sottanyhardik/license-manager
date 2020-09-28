a258f9569c0213088e8761a5a987b52f3903a9e2
Hardik <h.sottany@gmail.com>
2020-09-28 23:18:14 +0530

    View

--- core/views.py
+++ core/views.py
@@ -154,7 +154,7 @@ class UploadLedger(TemplateView):
         from django.http import HttpResponseRedirect
         from django.urls import reverse
         if license:
-            return HttpResponseRedirect(reverse('license_ledger_pdf', kwargs={'license': license}))
+            return HttpResponseRedirect(reverse('license_ledger', kwargs={'license': license}))
         else:
             return HttpResponseRedirect(reverse('ledger-upload'))
 

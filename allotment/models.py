41e79caec263d66fed56f3460f2bf45894be2d6a
Hardik <h.sottany@gmail.com>
2020-11-06 13:14:13 +0530

    ETA

--- allotment/models.py
+++ allotment/models.py
@@ -47,7 +47,7 @@ class AllotmentModel(models.Model):
 
     def __str__(self):
         if self.invoice:
-            return "{0} {1} {2} {3}".format(self.item_name, self.company.name, str(self.invoice),str(self.required_quantity))
+            return "{0} {1} {2} {3} {4}".format(self.item_name, self.company.name, str(self.invoice),str(self.required_quantity), self.eta)
         else:
             return "{0} {1} {2}".format(self.item_name, self.company.name,
                                             str(self.required_quantity))

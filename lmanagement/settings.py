6a914239808922b57e9b143e5f02cfd9867b7ea5
Your Name <you@example.com>
2023-03-31 09:28:35 +0000

    Settings

--- lmanagement/settings.py
+++ lmanagement/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'cn^fjh#*dhrjzrzphy!ic-2u())f(*wju3u_(06f^zq!g@_%o('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['139.59.1.170', 'localhost', '159.89.166.54']
+ALLOWED_HOSTS = ['128.199.22.9', 'localhost', '159.89.166.54','ksenterprises.agency']
 
 # Application definition
 

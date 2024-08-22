149f83ebaab32c9197f4203aaca426bd748510bc
Hardik Sottany <h.sottany@gmail.com>
2024-08-22 12:40:35 +0530

    Middleware

--- core/middleware.py
+++ core/middleware.py
@@ -7,19 +7,3 @@ class CrossOriginOpenerPolicyMiddleware:
         response = self.get_response(request)
         response['Cross-Origin-Opener-Policy'] = 'same-origin'
         return response
-
-
-# Example of setting CSP header in Django middleware
-
-class SecurityHeadersMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        response['Content-Security-Policy'] = (
-            "default-src 'self'; "
-            "style-src 'self' 'unsafe-inline'; "
-            "script-src 'self';"
-        )
-        return response
--- lmanagement/settings.py
+++ lmanagement/settings.py
@@ -62,7 +62,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'core.middleware.CrossOriginOpenerPolicyMiddleware',
-    'core.middleware.SecurityHeadersMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',

7b6232e9f6ca3f3fca211663ebf0c3a225bcce58
Hardik Sottany <hardik@purplehub.co>
2025-06-09 23:31:33 +0530

    hotfix: fetch and parse shipping bill data correctly from uploaded files

--- shipping_bill/urls.py
+++ shipping_bill/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:data>/dgft/fetch/', views.ShippingDGFTBillFetchView.as_view(), name='fetch_dgft_shipping_bill'),
+    path('upload/', views.UploadExcelShippingBillView.as_view(), name='upload_shipping_bill_excel'),
     path('<int:data>/fetch/', views.ShippingBillFetchView.as_view(), name='fetch_shipping_bill'),
     path('<int:data>/download/', views.ShippingBillDumpView.as_view(), name='download_shipping_bill'),
     path('<int:data>/list/', views.ShippingBillList.as_view(), name='shipping_bill_detail_list'),

b36a90c3aaf436ca025ebe3f4c5f4c0fbe00db6e
Hardik Sottany <h.sottany@gmail.com>
2026-07-17 12:08:27 +0530

    cleanup(inventory): remove dead allocation helpers

--- frontend/src/services/calculators/index.js
+++ frontend/src/services/calculators/index.js
@@ -2,8 +2,6 @@
  * Centralized export for all calculator utilities.
  */
 
-export * as allocationCalculator from './allocationCalculator';
 export * as formCalculator from './formCalculator';
 
-export {default as allocationCalculatorDefault} from './allocationCalculator';
 export {default as formCalculatorDefault} from './formCalculator';
